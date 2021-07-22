31d39b27e20164151d5479358d30e1bc94ed67f3
antoine <antoine@labard.fr>
2021-07-22 11:23:46 +0200

    Renamed user.dart to local_user.dart (due to a conflict with the Firebase User class).

--- lib/models/local_user.dart
+++ lib/models/local_user.dart
@@ -1,4 +1,4 @@
-class User {
+class LocalUser {
   String? uid = "";
   String? name = "";
   String? email = "";
@@ -7,7 +7,7 @@ class User {
   int? state = -1;
   String? profilePhoto = "";
 
-  User({
+  LocalUser({
     this.uid,
     this.name,
     this.email,
@@ -17,7 +17,7 @@ class User {
     this.profilePhoto,
   });
 
-  Map toMap(User user) {
+  Map toMap(LocalUser user) {
     var data = Map<String, dynamic>();
     data['uid'] = user.uid;
     data['name'] = user.name;
@@ -29,7 +29,7 @@ class User {
     return data;
   }
 
-  User.fromMap(Map<String, dynamic> mapData) {
+  LocalUser.fromMap(Map<String, dynamic> mapData) {
     this.uid = mapData['uid'];
     this.name = mapData['name'];
     this.email = mapData['email'];
