3ccbb9e8a001e7e0e4a046cbcd9f251610d0b047
antoine <antoine@labard.fr>
2021-07-24 18:28:54 +0200

    Partially implemented chat_screen_list.dart.

--- lib/screens/page_views/chat_screen_list.dart
+++ lib/screens/page_views/chat_screen_list.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/resources/firebase_repository.dart';
+import 'package:skype_flutter_clone/utils/Constants.dart';
+import 'package:skype_flutter_clone/utils/utils.dart';
+import 'package:skype_flutter_clone/widgets/custom_appbar.dart';
+import 'package:skype_flutter_clone/widgets/new_chat_button.dart';
+import 'package:skype_flutter_clone/widgets/user_circle.dart';
 
 class ChatScreenList extends StatefulWidget {
   const ChatScreenList({Key? key}) : super(key: key);
@@ -7,9 +13,53 @@ class ChatScreenList extends StatefulWidget {
   _ChatScreenListState createState() => _ChatScreenListState();
 }
 
+final FireBaseRepository _repository = FireBaseRepository();
+
 class _ChatScreenListState extends State<ChatScreenList> {
+  late String currentUserId;
+  late String initials;
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  void initState() {
+    super.initState();
+    _repository.getCurrentUser().then((user) {
+      setState(() {
+        currentUserId = user.uid;
+        initials = Utils.getInitials(user.displayName!);
+      });
+    });
   }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Constants.blackColor,
+        appBar: customAppbar(context),
+        floatingActionButton: NewChatButton(),
+      );
+
+  CustomAppbar customAppbar(BuildContext context) => CustomAppbar(
+        title: UserCircle(text: initials),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ))
+        ],
+        leading: IconButton(
+          icon: Icon(
+            Icons.notifications,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        centerTitle: true,
+      );
 }
