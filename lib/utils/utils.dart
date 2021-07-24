13bdc6241ef8cd9e4c907d9b66d808e39e6e7856
antoine <antoine@labard.fr>
2021-07-24 17:12:00 +0200

    Added getInitials.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,8 @@
 class Utils {
   static String getUsername(String email) => "live:${email.split('@')[0]}";
+
+  static String getInitials(String displayName) {
+    List<String> split = displayName.split(" ");
+    return split[0][0] + split[1][0];
+  }
 }
