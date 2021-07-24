eb5ff9171addd16eb0bae8d6276c6a47295b51cd
antoine <antoine@labard.fr>
2021-07-24 16:35:09 +0200

    Added custom_appbar.dart.

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatefulWidget {
+  const CustomAppbar({Key? key}) : super(key: key);
+
+  @override
+  _CustomAppbarState createState() => _CustomAppbarState();
+}
+
+class _CustomAppbarState extends State<CustomAppbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
