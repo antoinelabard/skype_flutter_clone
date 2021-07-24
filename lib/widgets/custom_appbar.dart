7b629f78bfa2d0db911d27edc0be71b7ac15b1cd
antoine <antoine@labard.fr>
2021-07-24 16:55:57 +0200

    Implemented custom_appbar.dart.

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/utils/Constants.dart';
 
-class CustomAppbar extends StatefulWidget {
-  const CustomAppbar({Key? key}) : super(key: key);
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget{
+
+  final Widget title;
+  final List<Widget> actions;
+  final Widget leading;
+  final bool centerTitle;
+
+  const CustomAppbar({Key? key,
+  required this.title,
+  required this.actions,
+  required this.leading,
+  required this.centerTitle,
+  }) : super(key: key);
 
   @override
-  _CustomAppbarState createState() => _CustomAppbarState();
+  Widget build(BuildContext context) => Container(
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: Constants.blackColor,
+      border: Border(
+        bottom: BorderSide(
+          color: Constants.separatorColor,
+          width: 1.4,
+          style: BorderStyle.solid
+        )
+      )
+    ),
+    child: AppBar(
+      backgroundColor: Constants.blackColor,
+elevation: 0,
+      title: title,
+      actions: actions,
+      leading: leading,
+      centerTitle: centerTitle,
+    ),
+  );
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight+10);
 }
 
-class _CustomAppbarState extends State<CustomAppbar> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
