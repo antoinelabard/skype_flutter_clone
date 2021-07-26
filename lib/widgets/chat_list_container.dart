d8d2e059f3bec2a67607a8cf699bc8afb61aaca8
antoine <antoine@labard.fr>
2021-07-26 15:57:24 +0200

    Implemented chat_list_container.dart.

--- lib/widgets/chat_list_container.dart
+++ lib/widgets/chat_list_container.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/utils/Constants.dart';
+
+import 'custom_tile.dart';
 
 class ChatListContainer extends StatefulWidget {
   final String currentUserId;
@@ -13,6 +16,50 @@ class ChatListContainer extends StatefulWidget {
 class _ChatListContainerState extends State<ChatListContainer> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ListView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: 2,
+          itemBuilder: (context, index) {
+            return CustomTile(
+              mini: false,
+              onTap: () {},
+              subtitle: Text(
+                "hello",
+                style: TextStyle(color: Constants.greyColor, fontSize: 14),
+              ),
+              leading: Container(
+                constraints: BoxConstraints(maxHeight: 60, maxWidth: 60),
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 30,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(
+                          "https://lh3.googleusercontent.com/a/AATXAJz_Sh-qOWbzw88o4tekKLG1grqNQQJ_W9z36cU=s96-c"),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Constants.onlineDotColor,
+                            border: Border.all(
+                                color: Constants.blackColor, width: 2)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              title: Text(
+                "Antoine Labard",
+                style: TextStyle(
+                    color: Colors.white, fontFamily: "Arial", fontSize: 19),
+              ),
+            );
+          }),
+    );
   }
 }
