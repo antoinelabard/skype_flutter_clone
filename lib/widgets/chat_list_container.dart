a1b8a2283e50ac94130b56a8b78501fecc6b38ce
antoine <antoine@labard.fr>
2021-07-26 15:19:37 +0200

    Added chat_screen_container.dart.

---
+++ lib/widgets/chat_list_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatListContainer extends StatefulWidget {
+  final String currentUserId;
+
+  const ChatListContainer({Key? key, required this.currentUserId})
+      : super(key: key);
+
+  @override
+  _ChatListContainerState createState() => _ChatListContainerState();
+}
+
+class _ChatListContainerState extends State<ChatListContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
