0380098bfd7705e69b42640988a01f9db6680792
antoine <antoine@labard.fr>
2021-07-27 18:26:35 +0200

    Added search_app_bar.dart.

---
+++ lib/widgets/search_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchAppbar extends StatelessWidget {
+  const SearchAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
