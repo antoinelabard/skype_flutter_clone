98d93c215d317a4fd38b8ddf03ab49653181d565
antoine <antoine@labard.fr>
2021-08-05 16:31:46 +0200

    Added modal_tile.dart

---
+++ lib/widgets/modal_tile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ModalTile extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final Icon iconData;
+
+  const ModalTile(
+      {Key? key,
+      required this.title,
+      required this.subTitle,
+      required this.iconData})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
