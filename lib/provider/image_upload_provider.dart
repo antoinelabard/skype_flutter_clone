a7392fc17e02b89a87c6d0b4297cc5c962909a50
antoine <antoine@labard.fr>
2021-08-09 15:42:49 +0200

    Implemented image_upload_provider.dart.

--- lib/provider/image_upload_provider.dart
+++ lib/provider/image_upload_provider.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:skype_flutter_clone/enum/view_state.dart';
 
+class ImageUploadProvider with ChangeNotifier {
+  var _viewState = ViewState.IDLE;
+
+  get getViewState => _viewState;
+
+  void setToLoading() {
+    _viewState = ViewState.LOADING;
+    notifyListeners();
+  }
+
+  void setToIdle() {
+    _viewState = ViewState.IDLE;
+    notifyListeners();
+  }
+}
