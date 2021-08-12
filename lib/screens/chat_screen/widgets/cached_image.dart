b7274ed08941ed9515217fdbf0820f53eb483cf0
antoine <antoine@labard.fr>
2021-08-12 10:34:54 +0200

    Implemented cached_image.dart.

--- lib/screens/chat_screen/widgets/cached_image.dart
+++ lib/screens/chat_screen/widgets/cached_image.dart
@@ -1,1 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 
+class CachedImage extends StatelessWidget {
+  final String url;
+
+  const CachedImage({Key? key, required this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5),
+      child: CachedNetworkImage(
+        imageUrl: url,
+        placeholder: (context, url) => Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
