558bc685cfa54eaafa89405eb70a4295084e7a73
Antoine Labard <antoine@labard.fr>
2021-08-16 10:32:43 +0200

    Implemented pickup_layout.dart.

--- lib/screens/call_screens/pickup/pickup_layout.dart
+++ lib/screens/call_screens/pickup/pickup_layout.dart
@@ -1,1 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skype_flutter_clone/models/call.dart';
+import 'package:skype_flutter_clone/provider/user_provider.dart';
+import 'package:skype_flutter_clone/resources/call_methods.dart';
+import 'package:skype_flutter_clone/screens/call_screens/pickup/pickup_screen.dart';
 
+class PickupLayout extends StatelessWidget {
+  final Widget scaffold;
+  final callMethods = Callmethods();
+
+  PickupLayout({Key? key, required this.scaffold}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+
+    return (userProvider.getUser() != null)
+        ? StreamBuilder<DocumentSnapshot>(
+            stream: callMethods.callStream(uid: userProvider.getUser().uid),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data != null) {
+                return PickupScreen(
+                    call: Call.fromMap(snapshot.data as Map<String, dynamic>));
+              }
+              return scaffold;
+            },
+          )
+        : Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+  }
+}
