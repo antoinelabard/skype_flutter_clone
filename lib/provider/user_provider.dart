0414a8c3784d3472d61f77959a69a8610599d3b6
Antoine Labard <antoine@labard.fr>
2021-08-16 10:32:24 +0200

    Implemented refreshUser method.

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:skype_flutter_clone/models/local_user.dart';
+import 'package:skype_flutter_clone/resources/firebase_repository.dart';
 
+class UserProvider with ChangeNotifier {
+  late LocalUser _user;
+
+  getUser() => _user;
+  FireBaseRepository _fireBaseRepository = FireBaseRepository();
+
+  void refreshUser() async {
+    LocalUser user = await _fireBaseRepository.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
