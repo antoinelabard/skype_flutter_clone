9a2f14efc01519a58814ef59f206cbec53dbf525
Antoine Labard <antoine@labard.fr>
2021-08-17 19:21:49 +0200

    Implemented permissions.dart.

---
+++ lib/utils/permissions.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class Permissions {
+  static Future<bool> cameraAndMicrophonePermissionsGranted() async {
+    PermissionStatus cameraPermissionStatus = await _getCameraPermission();
+    PermissionStatus microphonePermissionStatus =
+        await _getMicrophonePermission();
+
+    if (cameraPermissionStatus == PermissionStatus.granted &&
+        microphonePermissionStatus == PermissionStatus.granted) {
+      return true;
+    } else {
+      _handleInvalidPermissions(
+          cameraPermissionStatus, microphonePermissionStatus);
+      return false;
+    }
+  }
+
+  static Future<PermissionStatus> _getCameraPermission() async {
+    PermissionStatus permission = await Permission.camera.status;
+    if (permission != PermissionStatus.granted) {
+      var permissionStatus = await Permission.camera.request();
+      return permissionStatus;
+    } else {
+      return permission;
+    }
+  }
+
+  static Future<PermissionStatus> _getMicrophonePermission() async {
+    var permission = await Permission.microphone.status;
+    if (permission != PermissionStatus.granted) {
+      var permissionStatus = await Permission.camera.status;
+      return permissionStatus;
+    } else {
+      return permission;
+    }
+  }
+
+  static void _handleInvalidPermissions(
+    PermissionStatus cameraPermissionStatus,
+    PermissionStatus microphonePermissionStatus,
+  ) {
+    if (cameraPermissionStatus == PermissionStatus.denied &&
+        microphonePermissionStatus == PermissionStatus.denied) {
+      throw new PlatformException(
+          code: "PERMISSION_DENIED",
+          message: "Access to camera and microphone denied",
+          details: null);
+    } else {
+      throw new PlatformException(
+          code: "PERMISSION_DISABLED",
+          message: "Location data is not available on device",
+          details: null);
+    }
+  }
+}
