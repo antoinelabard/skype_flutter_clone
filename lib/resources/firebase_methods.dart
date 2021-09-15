b686226c544b4c02a7cd2684c8bfa50b903edc9c
Antoine Labard <antoine@labard.fr>
2021-09-15 16:55:54 +0200

    Updated firebase_methods.dart.

--- lib/resources/firebase_methods.dart
+++ lib/resources/firebase_methods.dart
@@ -1,79 +1,15 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:skype_flutter_clone/constants/strings.dart';
 import 'package:skype_flutter_clone/models/local_user.dart';
 import 'package:skype_flutter_clone/models/message.dart';
 import 'package:skype_flutter_clone/provider/image_upload_provider.dart';
-import 'package:skype_flutter_clone/utils/utils.dart';
 
 class FirebaseMethods {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  GoogleSignIn _googleSignIn = GoogleSignIn();
   static final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   late Reference _firebaseReference;
-  var _userCollection = firebaseFirestore.collection(USERS_COLLECTION);
-
-  Future<User> getCurrentUser() async {
-    User currentUser = _auth.currentUser!;
-    return currentUser;
-  }
-
-  Future<UserCredential> signIn() async {
-    GoogleSignInAccount? _googleSignInAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication _googleSignInAuthentication =
-        await _googleSignInAccount!.authentication;
-    final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: _googleSignInAuthentication.accessToken,
-        idToken: _googleSignInAuthentication.idToken);
-    UserCredential userCredential =
-        await _auth.signInWithCredential(credential);
-    return userCredential;
-  }
-
-  Future<bool> authenticateUser(User user) async {
-    QuerySnapshot result = await firebaseFirestore
-        .collection(USERS_COLLECTION)
-        .where(EMAIL_FIELD, isEqualTo: user.email)
-        .get();
-    final List<DocumentSnapshot> docs = result.docs;
-    return docs.length == 0;
-  }
-
-  Future<void> addDataToDb(User currentUser) async {
-    LocalUser user = LocalUser(
-        uid: currentUser.uid,
-        email: currentUser.email,
-        name: currentUser.displayName,
-        profilePhoto: currentUser.photoURL,
-        username: Utils.getUsername(currentUser.email!));
-    firebaseFirestore
-        .collection(USERS_COLLECTION)
-        .doc(user.uid)
-        .set(user.toMap());
-  }
-
-  Future<void> signOut() async {
-    _googleSignIn.disconnect();
-    _googleSignIn.signOut();
-    return await _auth.signOut();
-  }
-
-  Future<List<LocalUser>> fetchAllUser(User currentUser) async {
-    List<LocalUser> userList = [];
-    QuerySnapshot querySnapshot =
-        await firebaseFirestore.collection(USERS_COLLECTION).get();
-    for (var i = 0; i < querySnapshot.docs.length; i++) {
-      if (querySnapshot.docs[i].id != currentUser.uid) {
-        userList.add(LocalUser.fromMap(
-            querySnapshot.docs[i].data() as Map<String, dynamic>));
-      }
-    }
-    return userList;
-  }
 
   Future<void> addMessageToDb(
       Message message, LocalUser sender, LocalUser receiver) async {
@@ -132,11 +68,5 @@ class FirebaseMethods {
         .doc(_message.receiverId)
         .collection(_message.senderId)
         .add(_map);
-  }
-
-  Future<LocalUser> getUserDetails() async {
-    User currentUser = await getCurrentUser();
-    var documentSnapshot = await _userCollection.doc(currentUser.uid).get();
-    return LocalUser.fromMap(documentSnapshot.data()!);
   }
 }
