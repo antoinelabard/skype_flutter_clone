f7f3ce033f505bf554a4446cec7e54495584e600
Antoine Labard <antoine@labard.fr>
2021-09-15 16:57:00 +0200

    Updated firebase_repository.dart.

--- lib/resources/firebase_repository.dart
+++ lib/resources/firebase_repository.dart
@@ -9,20 +9,6 @@ import 'package:skype_flutter_clone/resources/firebase_methods.dart';
 class FireBaseRepository {
   FirebaseMethods _firebaseMethods = FirebaseMethods();
 
-  Future<User> getCurrentUser() => _firebaseMethods.getCurrentUser();
-
-  Future<UserCredential> signIn() => _firebaseMethods.signIn();
-
-  Future<bool> authenticateUser(User user) =>
-      _firebaseMethods.authenticateUser(user);
-
-  Future<void> addDataToDb(User user) => _firebaseMethods.addDataToDb(user);
-
-  Future<void> signOut() => _firebaseMethods.signOut();
-
-  Future<List<LocalUser>> fetchAllUsers(User currentUser) =>
-      _firebaseMethods.fetchAllUser(currentUser);
-
   Future<void> addMessageToDb(
           Message message, LocalUser sender, LocalUser receiver) async =>
       _firebaseMethods.addMessageToDb(message, sender, receiver);
@@ -34,6 +20,4 @@ class FireBaseRepository {
           required ImageUploadProvider imageUploadProvider}) =>
       _firebaseMethods.uploadImage(
           image, receiverId, senderId, imageUploadProvider);
-
-  Future<LocalUser> getUserDetails() async => _firebaseMethods.getUserDetails();
 }
