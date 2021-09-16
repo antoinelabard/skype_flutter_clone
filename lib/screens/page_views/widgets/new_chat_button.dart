bcb8e761c424407751c8cd5ff53270ad3b806077
Antoine Labard <antoine@labard.fr>
2021-09-16 11:53:51 +0200

    Added and implemented new_chat_button.dart.

---
+++ lib/screens/page_views/widgets/new_chat_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/utils/Constants.dart';
+
+class NewChatButton extends StatelessWidget {
+  const NewChatButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: Constants.fabGradient,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Icon(
+        Icons.edit,
+        color: Colors.white,
+        size: 25,
+      ),
+      padding: EdgeInsets.all(15),
+    );
+  }
+}
