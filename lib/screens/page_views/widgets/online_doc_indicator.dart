12ded6ee845df3f4c97405fb46a46d11aab924e8
Antoine Labard <antoine@labard.fr>
2021-09-17 17:12:31 +0200

    Added and implemented online_doc_indicator.dart.

---
+++ lib/screens/page_views/widgets/online_doc_indicator.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/enum/user_state.dart';
+import 'package:skype_flutter_clone/models/local_user.dart';
+import 'package:skype_flutter_clone/resources/auth_methods.dart';
+import 'package:skype_flutter_clone/utils/utils.dart';
+
+class OnlineDotIndicator extends StatelessWidget {
+  OnlineDotIndicator({Key? key, required this.uid}) : super(key: key);
+
+  final String uid;
+  final _authMethods = AuthMethods();
+
+  @override
+  Widget build(BuildContext context) {
+    getColor(int state) {
+      switch (Utils.numToState(state)) {
+        case UserState.Offline:
+          return Colors.red;
+        case UserState.Online:
+          return Colors.green;
+        default:
+          return Colors.orange;
+      }
+    }
+
+    return Align(
+      alignment: Alignment.topRight,
+      child: StreamBuilder<DocumentSnapshot>(
+          stream: _authMethods.getUserStream(uid: uid),
+          builder: (context, snapshot) {
+            late LocalUser user;
+            if (snapshot.hasData) {
+              user = LocalUser.fromMap(
+                  snapshot.data?.data() as Map<String, dynamic>);
+            }
+            return Container(
+              height: 10,
+              width: 10,
+              margin: EdgeInsets.only(right: 5, top: 5),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: getColor(user.state!)),
+            );
+          }),
+    );
+  }
+}
