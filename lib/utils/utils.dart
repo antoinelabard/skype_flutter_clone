083293a14d667dd4e6baa0c7ef13de8a0eecc1b3
Antoine Labard <antoine@labard.fr>
2021-09-17 14:13:11 +0200

    Added UserState to int conversion functions.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -5,6 +5,7 @@ import 'package:image/image.dart' as Im;
 import 'package:image_picker/image_picker.dart';
 import 'package:image_picker_platform_interface/src/types/image_source.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:skype_flutter_clone/enum/user_state.dart';
 
 class Utils {
   static String getUsername(String email) => "live:${email.split('@')[0]}";
@@ -28,5 +29,27 @@ class Utils {
     var i = Im.encodeJpg(image, quality: 85);
     File f = File('$path/img_$random.jpg')..writeAsBytesSync(i);
     return f;
+  }
+
+  static int stateTuNum(UserState userState) {
+    switch (userState) {
+      case UserState.Offline:
+        return 0;
+        case UserState.Online:
+        return 1;
+      default:
+        return 2;
+    }
+  }
+
+  static UserState numToState(int num) {
+    switch (num) {
+      case 0:
+        return UserState.Offline;
+      case 1:
+        return UserState.Online;
+      default:
+        return UserState.Waiting;
+    }
   }
 }
