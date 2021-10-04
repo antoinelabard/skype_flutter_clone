4708f1779c5e254bdcea623c4455790d941f9b2e
Antoine Labard <antoine@labard.fr>
2021-10-04 09:53:07 +0200

    Added and implemented shimmering_logo.dart.

---
+++ lib/screens/page_views/widgets/shimmering_logo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:skype_flutter_clone/utils/Constants.dart';
+
+class ShimmeringLogo extends StatelessWidget {
+  const ShimmeringLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      child: Shimmer.fromColors(child: Image.asset("assets/app_logo.png"), baseColor: Constants.blackColor, highlightColor: Colors.white),
+    );
+  }
+}
+
