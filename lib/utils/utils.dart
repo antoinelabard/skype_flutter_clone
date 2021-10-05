fde042fae640e3f02ec2651dd0b162fb095736b4
Antoine Labard <antoine@labard.fr>
2021-10-05 11:09:51 +0200

    Added a date format conversion method.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:image/image.dart' as Im;
 import 'package:image_picker/image_picker.dart';
 import 'package:image_picker_platform_interface/src/types/image_source.dart';
+import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:skype_flutter_clone/enum/user_state.dart';
 
@@ -51,5 +52,11 @@ class Utils {
       default:
         return UserState.Waiting;
     }
+  }
+
+  static String formatDateString(String dateString) {
+    var dateTime =DateTime.parse(dateString);
+    var formatter = DateFormat('dd/MM/yy');
+    return formatter.format(dateTime);
   }
 }
