72ca018166ac0ebfa0ff527bde8414cc85d72a5a
Antoine Labard <antoine@labard.fr>
2021-10-07 17:11:29 +0200

    Added a floating action button in log_screen.dart.

--- lib/widgets/logs/widgets/log_screen.dart
+++ lib/widgets/logs/widgets/log_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:skype_flutter_clone/models/log.dart';
 import 'package:skype_flutter_clone/resources/local_db/repository/log_repository.dart';
+import 'package:skype_flutter_clone/screens/call_screens/pickup/pickup_layout.dart';
 import 'package:skype_flutter_clone/utils/Constants.dart';
+import 'package:skype_flutter_clone/widgets/skype_appbar.dart';
+
+import 'floating_column.dart';
 
 class LogScreen extends StatelessWidget {
   const LogScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Constants.blackColor,
+    return PickupLayout(
+      scaffold: Scaffold(
+        appBar: SkypeAppbar(
+          title: "Calls",
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, "/search_screen");
+              },
+            ),
+          ],
+        ),
+        backgroundColor: Constants.blackColor,
         body: Center(
           child: TextButton(
             child: Text("Click me"),
@@ -19,7 +38,8 @@ class LogScreen extends StatelessWidget {
             },
           ),
         ),
+        floatingActionButton: FloatingColumn(),
+      ),
     );
   }
 }
-
