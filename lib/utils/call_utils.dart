4786638e8f56379c6dd95ff3699bad81c87e44ec
Antoine Labard <antoine@labard.fr>
2021-10-07 17:25:10 +0200

    Added logging function in call_utils.dart.

--- lib/utils/call_utils.dart
+++ lib/utils/call_utils.dart
@@ -1,9 +1,12 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/constants/strings.dart';
 import 'package:skype_flutter_clone/models/call.dart';
 import 'package:skype_flutter_clone/models/local_user.dart';
+import 'package:skype_flutter_clone/models/log.dart';
 import 'package:skype_flutter_clone/resources/call_methods.dart';
+import 'package:skype_flutter_clone/resources/local_db/repository/log_repository.dart';
 import 'package:skype_flutter_clone/screens/call_screens/call_screen.dart';
 
 class CallUtils {
@@ -24,5 +27,21 @@ class CallUtils {
       Navigator.push(context,
           MaterialPageRoute(builder: (context) => CallScreen(call: call)));
     }
+  var log = Log(
+    callerName: from.name!,
+    callerPic: from.profilePhoto!,
+    callStatus: CALL_STATUS_DIALLED,
+    receiverName: to.name!,
+    receiverPic: to.profilePhoto!,
+    timestamp: DateTime.now().toString()
+  );
+    var callMade = await callMethods.makeCall(call: call);
+
+    call.hasDialed = true;
+
+    if (callMade) {
+      LogRepository.addLogs(log);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => CallScreen(call: call)));
+          }
   }
 }
