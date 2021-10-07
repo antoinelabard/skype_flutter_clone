adc14a93d78152ba9b179e639961277dc6a8c8e0
Antoine Labard <antoine@labard.fr>
2021-10-07 17:10:46 +0200

    Added and implemented floating_column.dart.

---
+++ lib/widgets/logs/widgets/floating_column.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/utils/Constants.dart';
+
+class FloatingColumn extends StatelessWidget {
+  const FloatingColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: Constants.fabGradient
+          ),
+          child: Icon(
+            Icons.dialpad,
+            color: Colors.white,
+            size: 25,
+          ),
+          padding: EdgeInsets.all(15),
+        ),
+        SizedBox(height: 15,),
+        Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Constants.blackColor,
+            border: Border.all(
+              width: 2,
+              color: Constants.gradientColorEnd
+            )
+          ),child: Icon(
+          Icons.add_call,
+          color: Constants.gradientColorEnd,
+          size: 25,
+        ),
+          padding: EdgeInsets.all(15),
+        )
+      ],
+    );
+  }
+}
