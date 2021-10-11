7dd75f7476bb7d1ae8581d22ac488e22ef5596fa
Antoine Labard <antoine@labard.fr>
2021-10-11 12:30:46 +0200

    updated pickup_screen.dart to add a call logging functionality.

--- lib/screens/call_screens/pickup/pickup_screen.dart
+++ lib/screens/call_screens/pickup/pickup_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:skype_flutter_clone/constants/strings.dart';
 import 'package:skype_flutter_clone/models/call.dart';
+import 'package:skype_flutter_clone/models/log.dart';
 import 'package:skype_flutter_clone/resources/call_methods.dart';
+import 'package:skype_flutter_clone/resources/local_db/repository/log_repository.dart';
 import 'package:skype_flutter_clone/screens/call_screens/call_screen.dart';
 import 'package:skype_flutter_clone/screens/chat_screens/widgets/cached_image.dart';
 import 'package:skype_flutter_clone/utils/permissions.dart';
@@ -16,6 +19,28 @@ class PickupScreen extends StatefulWidget {
 
 class _PickupScreenState extends State<PickupScreen> {
   var callMethods = CallMethods();
+
+  addToLocalStorage({required String callStatus}) {
+    Log log = Log(
+        callerName: widget.call.callerName,
+        callerPic: widget.call.callerPic,
+        receiverName: widget.call.receiverName,
+        receiverPic: widget.call.receiverPic,
+        timestamp: DateTime.now().toString(),
+        callStatus: callStatus);
+
+    LogRepository.addLogs(log);
+  }
+
+  var isCallMissed = true;
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (isCallMissed) {
+      addToLocalStorage(callStatus: CALL_STATUS_MISSED);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,19 +70,26 @@ class _PickupScreenState extends State<PickupScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                    onPressed: () async =>
-                        await callMethods.endCall(call: widget.call),
+                    onPressed: () async {
+                      isCallMissed = false;
+                      addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
+                      await callMethods.endCall(call: widget.call);
+                    },
                     icon: Icon(Icons.call_end),
                     color: Colors.redAccent),
                 SizedBox(width: 25),
                 IconButton(
-                    onPressed: () async => await Permissions
-                            .cameraAndMicrophonePermissionsGranted()
-                        ? Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CallScreen(call: widget.call)))
-                        : {},
+                    onPressed: () async {
+                      isCallMissed = false;
+                      addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
+                      await Permissions.cameraAndMicrophonePermissionsGranted()
+                          ? Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CallScreen(call: widget.call)))
+                          : {};
+                    },
                     icon: Icon(Icons.call))
               ],
             )
