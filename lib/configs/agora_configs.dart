ade431eb6fdfb06ca1a1133f771f141cca9a0e47
Antoine Labard <antoine@labard.fr>
2021-10-25 09:39:35 +0200

    Added app screenshots.

--- lib/configs/agora_configs.dart
+++ lib/configs/agora_configs.dart
@@ -1,6 +1,6 @@
 const APP_ID = "313099884976438496952191904ed079";
 const APP_TOKEN =
-    "006313099884976438496952191904ed079IAC1ichheaIymCEONQintFLjKAlMh/VTgnSnZrZOUI4MTQx+f9gAAAAAEACOYDhkoIRqYQEAAQCghGph";
+    "006313099884976438496952191904ed079IAAvO9aVi/ftd2hkmSQyenpVP9MpX4YqVgILY5ejlQRuHwx+f9gAAAAAEABgmBKtFwtvYQEAAQAVC29h";
 // This constant is only useful while using a
 // temporary token for testing. Remove it and all its usages when deploying.
 const CHANNEL_ID = "test";
